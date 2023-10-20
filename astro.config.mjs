25737e54fb638d072f6a8ac58d93b9972c794794
hotair <warm5678@gmail.com>
2023-10-21 02:31:25 +0800

    2023-10-21-2:30

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site:'https://hotairkiller/TrendNewS.github.io',
+    site:'https://blog.github.io',
 });

58335791ef48e73348c36059feb1ce5d1b2729cc
hotair <warm5678@gmail.com>
2023-10-21 16:46:24 +0800

    2023-10-21-17:00

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel()
-} // site:'https://hotairkiller.github.io',
-);
+  site: 'https://hotairkiller.github.io',
+  integrations: [preact()]
+});

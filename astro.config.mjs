39b3194370571072ad8c3dbbaff7d5a06aeda692
hotair <warm5678@gmail.com>
2023-10-21 20:38:30 +0800

    2023-10-21:20:40

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-
+import netlify from '@astrojs/netlify';
 
 import preact from "@astrojs/preact";
 
-// https://astro.build/config
+
 export default defineConfig({
-  site: 'https://hotairkiller.github.io',
+  output:'server',
+  adapter:netlify(),
   
   integrations: [preact()]
 });
